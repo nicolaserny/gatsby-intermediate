6dd14ef18bd9918239268193db6bb3639f241a12
nicolaserny <nicolas.erny@xcomponent.com>
2020-06-03 17:24:41 +0200

    Pre bootstrap

---
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -0,0 +1,13 @@
+const withDefaults = require('./utils/default-options');
+const path = require('path');
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+
+exports.onPreBootstrap = ({ store }, options) => {
+  const { program } = store.getState();
+  const { contentPath } = withDefaults(options);
+  const dir = path.join(program.directory, contentPath);
+  if (!fs.existsSync(dir)) {
+    mkdirp.sync(dir);
+  }
+};
