487721427143e7234fd20eeb1ac3a3a131d8e32d
nicolaserny <nicolas.erny@xcomponent.com>
2020-06-12 16:58:56 +0200

    feat(lookup): Add results

--- sites/lookup/src/pages/search.js
+++ sites/lookup/src/pages/search.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import Form from '../components/form';
+import Results from '../components/results';
 
-const Search = () => (
-  <>
-    <h1>Search for Rick & Morty Character</h1>
-    <p>
-      Trying to remember which Rick you're talking about? Trying out this handy
-      search interface!
-    </p>
-    <Form />
-  </>
-);
+const Search = ({ location }) => {
+  const query =
+    (location.state && location.state.query) ||
+    location.pathname.replace(/^\/search\/?/, '') ||
+    '';
+  const name = query.replace(/-+/g, ' ');
+  return (
+    <>
+      <h1>
+        {name
+          ? `Showing  results for "${name}"`
+          : 'Search for Rick & Morty Character'}
+      </h1>
+      <p>
+        Trying to remember which Rick you're talking about? Trying out this
+        handy search interface!
+      </p>
+      <Form />
+      {name && <Results name={name} />}
+    </>
+  );
+};
 
 export default Search;
