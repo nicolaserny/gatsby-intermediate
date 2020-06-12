02d2c6dfea3489b3d7a2d4fcfc06d44d3a5a40cb
nicolaserny <nicolas.erny@xcomponent.com>
2020-06-12 15:33:34 +0200

    feat(lookup): setup client side

---
+++ sites/lookup/src/pages/search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = () => (
+  <>
+    <h1>Search for Rick & Morty Character</h1>
+    <p>
+      Trying to remember which Rick you're talking about? Trying out this handy
+      search interface!
+    </p>
+  </>
+);
+
+export default Search;
